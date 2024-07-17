58aa1fac3cbb67042b56c65418b5b79299a7dc7a
siddharth <siddhumyname@gmail.com>
2024-07-17 13:57:02 +0530

    minor changes

--- Skibbidy/src/Skibbidy/EntryPoint.h
+++ Skibbidy/src/Skibbidy/EntryPoint.h
@@ -6,6 +6,8 @@ extern Skibbidy::Application* Skibbidy::CreateApplication();
 int main(int argc, char** argv)
 {
 	auto app = Skibbidy::CreateApplication();
+	Skibbidy::Log::init();
+	Skibbidy::Log::GetCoreLogger()->warn("Nope");
 	app->Run();
 	delete app;
 }
